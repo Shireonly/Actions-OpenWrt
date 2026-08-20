c2d2438e37e8278276d7d3abed20840267676240
Shireonly <115936979+Shireonly@users.noreply.github.com>
2026-08-21 04:05:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 还原可能被误修改的 Patch 文件
+git checkout target/linux/ 2>/dev/null || true
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# 自动强行将内核源码及其补丁文件中的 20000 限制全量修改为 100
-#!/bin/bash
-# 修改默认 IP（可选）
-# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
-
-# 允许 netdev_budget_usecs 修改为小数值（解锁下限）
-find target/linux/ -name "sysctl_net_core.c" -exec sed -i 's/20000/100/g' {} + 2>/dev/null || true
-find net/core/ -name "sysctl.c" -exec sed -i 's/20000/100/g' {} + 2>/dev/null || true
+# 仅对编译解压后的实际 C 源码进行修改（不触碰任何 .patch 或 Makefile）
+find target/linux/ -name "sysctl_net_core.c" -o -name "sysctl.c" | while read -r file; do
+    sed -i '/netdev_budget_usecs/s/20000/100/g' "$file"
+done || true
